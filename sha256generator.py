a5cd83598dc75466da248f818bcbc6e44ef84a98
mutluexe <mutlu.donmez@hotmail.com>
2019-12-08 20:34:24 +0300

    update encoding

--- sha256generator.py
+++ sha256generator.py
@@ -1,15 +1,18 @@
 import hashlib
 import sys
+import io
 def main():
 
-    with open(sys.argv[1], "r") as file:
+    with io.open(sys.argv[1], "r", encoding='utf-8', errors='ignore') as file:
         f1 = file.readlines()
     for data in f1:
         print(encrypt_string(data))
 
+    file.close()
+
 def encrypt_string(string):
     sha_signature = \
-        hashlib.sha256(string.encode()).hexdigest()
+        hashlib.sha256(string.encode('utf-8', errors='ignore')).hexdigest()
     return sha_signature
 
 if __name__ == "__main__":
